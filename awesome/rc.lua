e5da9e398f82f3fe8dcc261c55ddd8197b723711
Pieter Joost van de Sande <pj@born2code.net>
2013-10-19 17:34:52 +0200

    Fix diskspace widget

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -181,7 +181,7 @@ vicious.register( batwidget, vicious.widgets.bat, '<span background="#92B0A0" fo
 fswidget = wibox.widget.textbox()
 
 vicious.register(fswidget, vicious.widgets.fs,
-'<span background="#D0785D" font="Inconsolata 11"> <span font="Inconsolata 11" color="#EEEEEE">${/home used_p}/${/home avail_p} GB </span></span>', 800)
+'<span background="#D0785D" font="Inconsolata 11"> <span font="Inconsolata 11" color="#EEEEEE">${/ used_gb} / ${/ avail_gb} GB </span></span>', 800)
 
 fsicon = wibox.widget.imagebox()
 fsicon:set_image(beautiful.fsicon)
