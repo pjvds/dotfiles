075f13844bc49709adfdbc2ae48a11ba3bf04cf4
Pieter Joost van de Sande <pj@born2code.net>
2019-01-16 08:48:53 +0100

    docker: alias d for docker

--- docker/init.zsh
+++ docker/init.zsh
@@ -1,7 +1,7 @@
+alias d='docker'
 alias dc='docker-compose'
 alias dcl='docker-compose logs -f'
 alias dcu='docker-compose up -d'
-
 export COMPOSE_PROJECT_NAME="pxr"
 
 alias ctop='docker run --rm -ti \
