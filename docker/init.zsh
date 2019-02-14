a13ae5972c3c65f3432c5cc245d4b8f2e7a00649
Pieter Joost van de Sande <pj@born2code.net>
2019-02-14 09:39:34 +0100

    docker: drop COMPOSE_PROJECT_NAME

--- docker/init.zsh
+++ docker/init.zsh
@@ -2,7 +2,6 @@ alias d='docker'
 alias dc='docker-compose'
 alias dcl='docker-compose logs -f'
 alias dcu='docker-compose up -d'
-export COMPOSE_PROJECT_NAME="pxr"
 
 alias ctop='docker run --rm -ti \
   --name=ctop \
