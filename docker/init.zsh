a87ac4d174b762eba9a6db0741964871bfd03dd6
Pieter Joost van de Sande <pj@born2code.net>
2019-02-19 01:26:16 +0100

    docker: add old dc logs alias

--- docker/init.zsh
+++ docker/init.zsh
@@ -2,6 +2,7 @@ alias d='docker'
 alias dc='docker-compose'
 alias dcl='docker-compose logs -f'
 alias dcu='docker-compose up -d'
+alias dcfl'docker-compose logs -f'
 
 alias ctop='docker run --rm -ti \
   --name=ctop \
