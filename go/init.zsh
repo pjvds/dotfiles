afed1733ec8b2d898529865799394e7860911f3d
Pieter Joost van de Sande <pj@born2code.net>
2019-05-23 23:36:51 +0200

    go: overwrite GOPATH after goenv init

--- go/init.zsh
+++ go/init.zsh
@@ -1,11 +1,13 @@
+if ! type goenv &> /dev/null; then
+  warn "missing goenv"
+  return
+fi
+eval "$(goenv init -)"
+
 export GOPATH="$HOME/go"
 export PATH="$PATH:$GOPATH/bin"
 export CGO_ENABLED=1
 export GOENV_ROOT="$HOME/.goenv"
 export PATH="$GOENV_ROOT/bin:$PATH"
 
-if ! type goenv &> /dev/null; then
-  warn "missing goenv"
-  return
-fi
-eval "$(goenv init -)"
+
