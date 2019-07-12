f1d8fda84455b130017e0eca57a1789a781d9125
Pieter Joost <pj@born2code.net>
2019-07-12 09:36:11 +0200

    bin: add flatbuffers and terraform to path

--- bin/init.zsh
+++ bin/init.zsh
@@ -1,1 +1,4 @@
 export PATH="$PATH:$DOTFILES_HOME/bin"
+export PATH="$PATH:$DOTFILES_HOME/bin/terraform_0.12.0_linux_amd64"
+export PATH="$PATH:$DOTFILES_HOME/bin/flatbuffers/flatbuffers-1.11.0"
+#export PATH="$PATH:$DOTFILES_HOME/bin/terraform_0.11.14_linux_amd64"
