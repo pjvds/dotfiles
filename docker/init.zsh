f7a95a6c4d1c568c5020c231e3389c6f9888508a
Pieter Joost <pvandesande@deloitte.nl>
2019-07-30 23:12:56 +0200

    docker: add dcps alias

--- docker/init.zsh
+++ docker/init.zsh
@@ -3,6 +3,7 @@ alias dc='docker-compose'
 alias dcl='docker-compose logs -f'
 alias dcu='docker-compose up -d'
 alias dcfl'docker-compose logs -f'
+alias dcps='docker-compose ps'
 
 alias ctop='docker run --rm -ti \
   --name=ctop \
