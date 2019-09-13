28a60cb2feed190cdf50572101b9a3004c7954df
Pieter Joost <pvandesande@deloitte.nl>
2019-09-13 10:58:40 +0200

    zsh: add gapa alias

--- zsh/init.zsh
+++ zsh/init.zsh
@@ -2,5 +2,7 @@ bindkey "^k" history-beginning-search-backward
 bindkey "^j" history-beginning-search-forward
 bindkey -s '^f' 'fg^M'
 
+alias gapa='git add -p'
+
 mkcd() { mkdir -p "$1" && cd "$1"; } 
 
