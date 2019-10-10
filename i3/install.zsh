9732960f36dbdf72ffe0b3e4b9b5bbe203afcff9
Pieter Joost <pvandesande@deloitte.nl>
2019-10-10 21:13:03 +0200

    i3: fix installation of config files

--- i3/install.zsh
+++ i3/install.zsh
@@ -2,4 +2,4 @@ mkdir -p $DOTFILES/.config/i3
 ln --symbolic $DOTFILES/i3/i3.config $HOME/.config/i3/config
 
 mkdir -p $DOTFILES/.config/i3status
-ln --symbolic $DOTFILES/i3status/i3status.config $HOME/.config/i3status/config
+ln --symbolic $DOTFILES/i3/i3status.config $HOME/.config/i3status/config
