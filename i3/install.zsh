c521ee24aa59fca79abb572695822b601cc23fb5
Pieter Joost <pvandesande@deloitte.nl>
2019-10-10 13:12:49 +0200

    i3: add i3status config

--- i3/install.zsh
+++ i3/install.zsh
@@ -1,1 +1,5 @@
-ln --symbolic $DOTFILES/i3/config $HOME/.config/i3/config
+mkdir -p $DOTFILES/.config/i3
+ln --symbolic $DOTFILES/i3/i3.config $HOME/.config/i3/config
+
+mkdir -p $DOTFILES/.config/i3status
+ln --symbolic $DOTFILES/i3status/i3status.config $HOME/.config/i3status/config
