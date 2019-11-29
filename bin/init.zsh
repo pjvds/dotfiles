c9ad9599931ad2592202deb470cf5f74e61b7fc7
Pieter Joost <pj@born2code.net>
2019-11-29 09:33:50 +0100

    bin: add Goland and Rider to path

--- bin/init.zsh
+++ bin/init.zsh
@@ -1,4 +1,7 @@
 export PATH="$PATH:$DOTFILES_HOME/bin"
 export PATH="$PATH:$DOTFILES_HOME/bin/terraform_0.12.0_linux_amd64"
 export PATH="$PATH:$DOTFILES_HOME/bin/flatbuffers/flatbuffers-1.11.0"
+export PATH="$PATH:$DOTFILES_HOME/bin/GoLand-2019.1.3/bin"
+export PATH="$PATH:$DOTFILES_HOME/bin/JetBrains Rider-2019.2.2/bin"
+
 #export PATH="$PATH:$DOTFILES_HOME/bin/terraform_0.11.14_linux_amd64"
