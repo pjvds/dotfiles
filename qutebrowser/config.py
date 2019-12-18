1733c0b277069c8d2246484f7baf55018f6924f4
Pieter Joost <pj@born2code.net>
2019-12-18 14:33:52 +0100

    qute: auto config

--- qutebrowser/config.py
+++ qutebrowser/config.py
@@ -5,5 +5,4 @@
 
 ## This is here so configs done via the GUI are still loaded.
 ## Remove it to not load settings done via the GUI.
-# config.load_autoconfig()
-config.bind('pl', "spawn --userscript qute-lastpass")
+config.load_autoconfig()
