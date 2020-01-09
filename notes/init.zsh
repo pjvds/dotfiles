15785a156f3f00db8487722ca8f6c786ec34f5db
Pieter Joost <pj@born2code.net>
2020-01-09 12:46:11 +0100

    notes: introduce notes sync function

---
+++ notes/init.zsh
@@ -0,0 +1,14 @@
+#!/bin/bash
+sync() {
+  gstatus=`git status --porcelain`
+
+  if [ ${#gstatus} -ne 0 ]
+  then
+
+      git add --all
+      git commit -m "$gstatus"
+
+      git pull
+      git push
+  fi
+}
