e390416bc0a7c8b0a25ed8a707baa25968c3b67e
Pieter Joost <pj@born2code.net>
2020-07-06 14:00:46 +0200

    k8s: add k3d lazy loading

--- k8/init.zsh
+++ k8/init.zsh
@@ -1,3 +1,20 @@
 alias k="kubectl --namespace dev-namespace"
 alias kd="kubectl --namespace dev-namespace"
 alias kt="kubectl --namespace test-namespace"
+
+# Check if 'k3d' is a command in $PATH
+if [ $commands[k3d] ]; then
+
+  # Placeholder 'k3d' shell function:
+  # Will only be executed on the first call to 'k3d'
+  k3d() {
+    # Remove this function, subsequent calls will execute 'kubectl' directly
+    unfunction "$0"
+
+    # Load auto-completion
+    eval $(k3d completion zsh)
+
+    # Execute 'kubectl' binary
+    $0 "$@"
+  }
+fi
