3be10465a056c0022b0f29410fec74edc5e9cc44
Pieter Joost <pj@born2code.net>
2020-10-06 12:52:39 +0200

    hstr: hello improved history searching

---
+++ hstr/init.zsh
@@ -0,0 +1,11 @@
+#!/bin/zsh
+if ! command -v hstr &> /dev/null
+then
+  warn "hstr missing!"
+  return
+fi
+
+alias hh=hstr                         # hh to be alias for hstr
+setopt histignorespace                # skip cmds w/ leading space from history
+export HSTR_CONFIG=hicolor            # get more colors
+bindkey -s "\C-r" "\C-a hstr -- \C-j" # bind hstr to Ctrl-r (for Vi mode check doc)
