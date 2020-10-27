8fe8bd5931818091541aade35f8e1332fe0effb8
Pieter Joost <pj@born2code.net>
2020-10-27 10:20:14 +0100

    node: lazy load nvm

--- node/init.zsh
+++ node/init.zsh
@@ -1,3 +1,12 @@
-export NODE_ENV=dev
+# Placeholder 'nvm' shell function:
+# Will only be executed on the first call to 'nvm'
+nvm() {
+  # Remove this function, subsequent calls will execute 'nvm' directly
+  unfunction "$0"
 
-source /usr/share/nvm/init-nvm.sh
+  source /usr/share/nvm/init-nvm.sh
+
+  # Execute binary
+  $0 "$@"
+}
+
