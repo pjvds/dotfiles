a1a4dc52ae8bf489fb4914c214ffb5db3fd37c71
Pieter Joost <pj@born2code.net>
2020-11-01 11:51:00 +0100

    picom: add picom module

---
+++ picom/install.zsh
@@ -0,0 +1,2 @@
+mkdir -p $DOTFILES/.config/
+ln -f --symbolic $DOTFILES/picom/picom.conf $HOME/.config/picom.conf
--- picon/install.zsh
+++
@@ -1,2 +0,0 @@
-mkdir -p $DOTFILES/.config/
-ln -f --symbolic $DOTFILES/compton/compton.conf $HOME/.config/compton.conf
