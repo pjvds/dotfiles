2d1833bffeebb6694d86767d91118ab4bd9b986d
Pieter Joost <pj@born2code.net>
2020-11-04 19:55:06 +0100

    arch: update package list

--- fzf/aliases.zsh
+++ fzf/aliases.zsh
@@ -1,7 +1,16 @@
 #source ~/.zplug/repos/junegunn/fzf/shell/key-bindings.zsh
 #source ~/.zplug/repos/junegunn/fzf/shell/completion.zsh
 
-zinit pack for fzf
+#zinit ice wait
+#zinit pack for fzf
+
+#zinit ice wait src'zsh/fzf-zsh-completion.sh'
+
 #source ~/.zplug/repos/lincheney/fzf-tab-completion/zsh/fzf-zsh-completion.sh
-zstyle ':completion:*' fzf-search-display true
-bindkey '^I' fzf_completion
+zinit lucid as=program pick="$ZPFX/bin/(fzf|fzf-tmux)" \
+    atclone="cp shell/completion.zsh _fzf_completion; \
+      cp bin/(fzf|fzf-tmux) $ZPFX/bin" \
+    make="PREFIX=$ZPFX install" for \
+        junegunn/fzf
+
+zinit light Aloxaf/fzf-tab
--- fzf/init.zsh
+++ fzf/init.zsh
@@ -7,4 +7,15 @@
 
 
 export FZF_DEFAULT_OPTS="--bind 'btab:toggle-up,tab:toggle-down' --cycle"
-zinit load junegunn/fzf-bin
+zinit pack for fzf
+#zinit light Aloxaf/fzf-tab
+
+# BIND MULTIPLE WIDGETS USING FZF
+zinit ice lucid wait'0c' multisrc"shell/{completion,key-bindings}.zsh" id-as"junegunn/fzf_completions" pick"/dev/null"
+#zinit light junegunn/fzf
+# FZF-TAB
+zinit ice wait"1" lucid
+zinit light Aloxaf/fzf-tab
+# SYNTAX HIGHLIGHTING
+zinit ice wait"0c" lucid atinit"zpcompinit;zpcdreplay"
+zinit light zdharma/fast-syntax-highlighting
