737be783842496108d84572c6a4890805e9c6584
Pieter Joost <pj@born2code.net>
2020-11-10 08:53:54 +0100

    misc: dir change aliasses

--- misc/init.zsh
+++ misc/init.zsh
@@ -3,6 +3,9 @@ alias .='cd $DOTFILES'
 alias .v='cd $DOTFILES && vim .'
 alias ls='ls --color'
 alias l='ls -lah'
+alias ..='cd ../'
+alias ...='cd ../../'
+alias ....='cd ../../../'
 
 zinit light pjvds/zsh-cwd
 cwd
