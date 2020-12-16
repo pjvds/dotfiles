6a60d12613b554849bda0fd2eb378d04231ed421
Pieter Joost <pj@born2code.net>
2020-12-16 18:38:38 +0100

    misc: add senv and cf function

--- misc/init.zsh
+++ misc/init.zsh
@@ -11,3 +11,35 @@ zinit light pjvds/zsh-cwd
 cwd
 
 zinit light pjvds/zsh-cd-print
+
+cf() {
+	selection=$(fzf)
+	if [ $? -eq 0 ]; then
+		cd $(dirname "$selection")
+	fi
+}
+
+# source .env file
+senv() {
+	local file=$([ -z "$1" ] && echo ".env" || echo "$1.env")
+	if [ ! -f "$file" ]
+	then
+		echo "no $file file found" 1>&2
+		exit 1
+	fi
+
+	local temp=$(mktemp -d)
+	local before="$temp/before"
+	local after="$temp/after"
+
+	#trap "rm \"$temp\"" INT TERM
+	export > $before
+
+	set -a
+	source "$file"
+	set +a
+
+	export > $after
+
+	sdiff "$before" "$after"
+}
