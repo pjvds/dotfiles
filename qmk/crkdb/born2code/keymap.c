092c1fde291dc52abcac1226e617cc4cefa2a913
Pieter Joost van de Sande <pj@born2code.net>
2021-04-28 11:09:29 +0200

    qmk: put brackets on raise layers home row

--- qmk/crkdb/born2code/keymap.c
+++ qmk/crkdb/born2code/keymap.c
@@ -38,9 +38,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [1] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-       KC_TILD, KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC,                      KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC,
+       KC_TILD, KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC,                     KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_DEL, KC_UNDS, KC_MINS, KC_PPLS, KC_EQL, XXXXXXX,                      KC_LEFT, KC_DOWN,   KC_UP,KC_RIGHT, XXXXXXX, KC_PIPE,
+      KC_DEL, KC_UNDS, KC_MINS, KC_PPLS, KC_EQL, XXXXXXX,                        KC_LEFT, KC_DOWN,   KC_UP,KC_RIGHT, XXXXXXX, KC_PIPE,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       KC_LSFT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      XXXXXXX, KC_LCBR, KC_LBRC, KC_RBRC, KC_RCBR, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
@@ -50,9 +50,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [2] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-       KC_GRAVE,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                         KC_6,    KC_7,    KC_8,    KC_9,    KC_0, KC_DEL,
+       KC_GRAVE,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                    KC_6   ,    KC_7,    KC_8,    KC_9,    KC_0, KC_DEL,
  //|--------+--------+--------+--------+--------+---------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LCTL, KC_F1,  KC_F2,    KC_F3,   KC_F4,   KC_F5,                      KC_F6,  KC_EQL, KC_LBRC, KC_RBRC, KC_BSLS,  KC_BSLS,
+      KC_LCTL, KC_F1,  KC_F2,    KC_F3,   KC_F4,   KC_F5,                        KC_F6  , KC_LCBR, KC_LBRC, KC_RBRC, KC_RCBR, KC_BSLS,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       KC_LSFT, KC_F7,  KC_F8,  KC_F9, KC_F10, KC_F11,                     KC_F12 , KC_MPRV, KC_MPLY, KC_MNXT, KC_PIPE, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
