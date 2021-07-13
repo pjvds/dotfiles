6c13f4f544be59a8440da3594975938607622434
Pieter Joost van de Sande <pj@born2code.net>
2021-07-13 13:53:20 +0200

    qmk: ignore mod tap interupt

--- qmk/crkdb/born2code/config.h
+++ qmk/crkdb/born2code/config.h
@@ -33,6 +33,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define TAPPING_FORCE_HOLD
 #define TAPPING_TERM 150
 
+#define IGNORE_MOD_TAP_INTERRUPT
+
 #ifdef RGBLIGHT_ENABLE
     #undef RGBLED_NUM
     #define RGBLIGHT_ANIMATIONS
