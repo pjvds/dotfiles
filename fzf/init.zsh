698061352496d932e28880eb840dfa2ba48fed15
Pieter Joost van de Sande <pj@born2code.net>
2021-07-15 08:46:30 +0200

    fzf: add zsh-fzf-up plugin

--- fzf/init.zsh
+++ fzf/init.zsh
@@ -18,3 +18,6 @@ export FZF_DEFAULT_COMMAND='ag --hidden --ignore .git -g ""'
 	# ** Completion
 source /usr/share/fzf/key-bindings.zsh
 source /usr/share/fzf/completion.zsh
+
+zinit ice silent depth=1 wait"1"
+zinit light "pjvds/zsh-fzf-up"
