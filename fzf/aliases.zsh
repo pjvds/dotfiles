6de1d0a8ca62c6dab3131c9ed58fbfa8759a4e42
Pieter Joost van de Sande <pj@born2code.net>
2021-08-06 16:59:26 +0200

    fzf: remove unnecessary sources and plugins

--- fzf/aliases.zsh
+++
@@ -1,7 +0,0 @@
-#source ~/.zplug/repos/junegunn/fzf/shell/key-bindings.zsh
-#source ~/.zplug/repos/junegunn/fzf/shell/completion.zsh
-
-#zinit ice wait
-#zinit pack for fzf
-
-#zinit ice wait src'zsh/fzf-zsh-completion.sh'
