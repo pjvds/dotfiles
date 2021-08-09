9e549228678d71b8a871d64276e0ff417175ac99
Pieter Joost van de Sande <pj@born2code.net>
2021-08-09 10:26:54 +0200

    node: turbo load nvm and remove custom lazy loading

--- node/init.zsh
+++ node/init.zsh
@@ -3,32 +3,5 @@ export NVM_LAZY_LOAD=true
 # disable browser launch after npm/yarn start
 export BROWSER=none
 
-#source /usr/share/nvm/init-nvm.sh
-v() {
-  source /usr/share/nvm/init-nvm.sh
-
-  # Execute binary
-  $EDITOR "$@"
-}
-
-# Placeholder 'nvm' shell function:
-# Will only be executed on the first call to 'nvm'
-nvm() {
-  # Remove this function, subsequent calls will execute 'nvm' directly
-  unfunction "$0"
-
-  source /usr/share/nvm/init-nvm.sh
-
-  # Execute binary
-  $0 "$@"
-}
-
-npm() {
-  # Remove this function, subsequent calls will execute 'nvm' directly
-  unfunction "$0"
-
-  source /usr/share/nvm/init-nvm.sh
-
-  # Execute binary
-  $0 "$@"
-}
+zinit ice wait"1" lucid
+zinit load /usr/share/nvm
