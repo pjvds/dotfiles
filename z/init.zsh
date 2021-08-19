16c0a438393fc40030c973e44971582b93779a62
Pieter Joost van de Sande <pj@born2code.net>
2021-08-19 10:26:51 +0200

    z: enable uncommon matching

--- z/init.zsh
+++ z/init.zsh
@@ -1,1 +1,5 @@
+# enable uncommon matching
+# this means if we search for `z code` we jump to `~/code` instead
+# of the most popular subdirectory of `~/code` like `~/code/myproject`
+export ZSHZ_UNCOMMON=1
 zinit load agkozak/zsh-z
