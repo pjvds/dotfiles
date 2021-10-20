abe59301de98eb659b56eb4f05787b0b42753094
Pieter Joost van de Sande <pj@born2code.net>
2021-10-20 10:42:54 +0200

    k8s: add auto completion to minikube

--- k8s/init.zsh
+++ k8s/init.zsh
@@ -2,6 +2,25 @@ alias k="kubectl --namespace dev-namespace"
 alias kd="kubectl --namespace dev-namespace"
 alias kt="kubectl --namespace test-namespace"
 
+# Check if 'minikube' is a command in $PATH
+if [ $commands[minikube] ]; then
+
+  # Placeholder 'minikube' shell function:
+  # Will only be executed on the first call to 'k3d'
+  minikube() {
+    # Remove this function, subsequent calls will execute 'kubectl' directly
+    unfunction "$0"
+
+    # Source the auto-completion
+    source <(minikube completion zsh | sed --expression='s/aliashash\["\([a-z]*\)"\]/aliashash[\1]/g')
+
+    # Execute binary
+    $0 "$@"
+  }
+fi
+
+
+
 # Check if 'k3d' is a command in $PATH
 if [ $commands[k3d] ]; then
 
