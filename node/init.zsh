48f140b941cf2221e35afc5c9090ca03e4815713
Pieter Joost van de Sande <pj@born2code.net>
2023-01-05 10:06:59 +0100

    node: add vitest and sst alias

--- node/init.zsh
+++ node/init.zsh
@@ -3,5 +3,8 @@ export NVM_LAZY_LOAD=true
 # disable browser launch after npm/yarn start
 export BROWSER=none
 
+alias vitest='npx vitest'
+alias sst='npx sst'
+
 zinit ice wait"1" lucid
 zinit load /usr/share/nvm
