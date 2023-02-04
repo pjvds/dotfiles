02a8116bd371c9a7036269743ad365cebb741a01
Pieter Joost van de Sande <pj@born2code.net>
2023-02-04 22:42:22 +0100

    node: support darwin os

---
+++ node/init.darwin.zsh
@@ -0,0 +1,2 @@
+[ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && \. "/opt/homebrew/opt/nvm/nvm.sh"  # This loads nvm
+[ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
--- node/init.zsh
+++ node/init.zsh
@@ -5,11 +5,3 @@ export BROWSER=none
 
 alias vitest='npx vitest'
 alias sst='npx sst'
-
-if [[ `uname` == "Darwin" ]]; then
-  [ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && \. "/opt/homebrew/opt/nvm/nvm.sh"  # This loads nvm
-  [ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
-else
-  zinit ice wait"1" lucid
-  zinit load /usr/share/nvm
-fi
