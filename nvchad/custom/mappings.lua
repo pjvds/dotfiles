2ceb596762d423892bee8603b68c1303cee1ef7c
Pieter Joost van de Sande <pj@born2code.net>
2023-02-08 21:49:50 +0100

    nvchad: configure navigation key mappings

--- nvchad/custom/mappings.lua
+++ nvchad/custom/mappings.lua
@@ -1,9 +1,18 @@
 local M = {}
 
+M.general = {
+  n = {
+    [";"] = { ":", "enter command mode", opts = { nowait = true } },
+  }
+}
+
 M.telescope = {
   n = {
+    ["<leader>a"] = { "<cmd> Telescope keymaps<CR>", "key mappings finder" },
+    ["<leader>f"] = { "<cmd> Telescope live_grep<CR>", "search in files" },
     ["<leader>p"] = { "<cmd> Telescope find_files<CR>", "file finder" },
-    ["<leader>e"] = { "<cmd> Telescope live_grep<CR>", "telescope live grep" },
+    ["<leader>e"] = { "<cmd> Telescope find_files<CR>", "file finder" },
+    ["<leader>j"] = { function() vim.diagnostic.goto_next({severity = vim.diagnostic.severity.ERROR}) end, "goto next diagnostics error" },
   },
 }
 
