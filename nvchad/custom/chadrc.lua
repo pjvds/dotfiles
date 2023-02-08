029aed64cd0a72b4662ca0d9ec9467921371c26c
Pieter Joost van de Sande <pj@born2code.net>
2023-02-08 21:48:31 +0100

    nvchad: configure lsp for typescript and go

--- nvchad/custom/chadrc.lua
+++ nvchad/custom/chadrc.lua
@@ -4,8 +4,7 @@ local M = {}
 --	theme_toggle = { "ayu-dark", "aye-light" },
 --	theme = "aye-dark",
 --}
-
---M.plugins = require "custom.plugins"
+M.plugins = require "custom.plugins"
 M.mappings = require "custom.mappings"
 
 return M
---
+++ nvchad/custom/plugins/lspconfig.lua
@@ -0,0 +1,17 @@
+-- custom.plugins.lspconfig
+local on_attach = require("plugins.configs.lspconfig").on_attach
+local capabilities = require("plugins.configs.lspconfig").capabilities
+
+local lspconfig = require "lspconfig"
+
+-- hint: all lang server names can be found here:
+--       https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
+-- or by: `:help lspconfig-all`
+local servers = { "tsserver", "gopls" }
+
+for _, lsp in ipairs(servers) do
+  lspconfig[lsp].setup {
+    on_attach = on_attach,
+    capabilities = capabilities,
+  }
+end
