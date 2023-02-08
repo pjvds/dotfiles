321952316ad7d9c976209da3c6ee3258482851ae
Pieter Joost van de Sande <pj@born2code.net>
2023-02-08 21:49:13 +0100

    kitty: reload kitty when config is changed via Vim

--- nvchad/custom/init.lua
+++ nvchad/custom/init.lua
@@ -1,1 +1,9 @@
 vim.g.mapleader = "'"
+
+local autocmd = vim.api.nvim_create_autocmd
+
+-- Send SIGUSR1 signal to kitty after config file changed to reload the configuration
+autocmd("BufWritePost", {
+  pattern = "*/kitty.conf",
+  command = "!killall -SIGUSR1 kitty",
+})
