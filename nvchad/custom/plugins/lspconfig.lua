5838d494e990d8346b7f8b5b853496d2c710b406
Pieter Joost van de Sande <pj@born2code.net>
2023-02-08 23:23:13 +0100

    nvchad: configure auto formatting with null-ls

--- nvchad/custom/plugins/lspconfig.lua
+++ nvchad/custom/plugins/lspconfig.lua
@@ -7,7 +7,7 @@ local lspconfig = require "lspconfig"
 -- hint: all lang server names can be found here:
 --       https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
 -- or by: `:help lspconfig-all`
-local servers = { "tsserver", "gopls" }
+local servers = { "tsserver", "gopls", "sumneko_lua", "jsonls" }
 
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
