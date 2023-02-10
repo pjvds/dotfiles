a4c7cf90d48018c7b221f5dabf28faecdb897f27
Pieter Joost van de Sande <pj@born2code.net>
2023-02-10 22:52:01 +0100

    nvchad: leader+a for diagnostic actions

--- nvchad/custom/mappings.lua
+++ nvchad/custom/mappings.lua
@@ -7,13 +7,31 @@ M.general = {
 }
 
 M.telescope = {
-  n = {
-    ["<leader>a"] = { "<cmd> Telescope keymaps<CR>", "key mappings finder" },
-    ["<leader>f"] = { "<cmd> Telescope live_grep<CR>", "search in files" },
-    ["<leader>p"] = { "<cmd> Telescope find_files<CR>", "file finder" },
-    ["<leader>e"] = { "<cmd> Telescope find_files<CR>", "file finder" },
-    ["<leader>j"] = { function() vim.diagnostic.goto_next({severity = vim.diagnostic.severity.ERROR}) end, "goto next diagnostics error" },
-  },
+	n = {
+		["<leader>A"] = { "<cmd> Telescope keymaps<CR>", "key mappings finder" },
+		["<leader>f"] = { "<cmd> Telescope live_grep<CR>", "search in files" },
+		["<leader>p"] = { "<cmd> Telescope find_files<CR>", "file finder" },
+		["<leader>e"] = { "<cmd> Telescope find_files<CR>", "file finder" },
+		["<leader>j"] = {
+			function()
+				vim.diagnostic.goto_next({
+					-- call vim.diagnostic.open_float() after moving.
+					float = true,
+					-- loop around file
+					wrap = true,
+					-- jump to at least warning level or higher
+					severity = { min = vim.diagnostic.severity.WARN },
+				})
+			end,
+			"goto next diagnostics error",
+		},
+		["<leader>a"] = {
+			function()
+				vim.lsp.buf.code_action()
+			end,
+			"show lsp code actions for current buffer",
+		},
+	},
 }
 
 return M
