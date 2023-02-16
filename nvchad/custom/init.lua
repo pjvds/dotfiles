a3dc7b1cf5d0e8b1201b4a0daf4f1cc5664a3f6e
Pieter Joost van de Sande <pj@born2code.net>
2023-02-16 20:57:40 +0100

    vim: don't use system clipboard

--- nvchad/custom/init.lua
+++ nvchad/custom/init.lua
@@ -1,9 +1,12 @@
 vim.g.mapleader = "'"
 
+-- don't use system clipboard
+vim.opt.clipboard = ""
+
 local autocmd = vim.api.nvim_create_autocmd
 
 -- Send SIGUSR1 signal to kitty after config file changed to reload the configuration
 autocmd("BufWritePost", {
-  pattern = "*/kitty.conf",
-  command = "!killall -SIGUSR1 kitty",
+	pattern = "*/kitty.conf",
+	command = "!killall -SIGUSR1 kitty",
 })
