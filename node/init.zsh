d733faa76d6cbd3350402b246e805c2a6a277736
Pieter Joost van de Sande <pj@born2code.net>
2023-03-01 23:03:10 +0100

    node: disable nvm lazy loading

--- node/init.zsh
+++ node/init.zsh
@@ -1,10 +1,8 @@
-export NVM_LAZY_LOAD=true
-
 # disable browser launch after npm/yarn start
 export BROWSER=none
 
 alias vitest='npx vitest'
 alias sst='npx sst'
 
-export NVM_LAZY_LOAD=true
+export NVM_LAZY_LOAD=false
 zinit light lukechilds/zsh-nvm
