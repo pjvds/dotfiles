6b9d4e4e0f52f80d13bc86116be33022495f28da
Pieter Joost van de Sande <pj@born2code.net>
2023-04-11 15:51:50 +0200

    node: use pnpx for sst and vitest

--- node/init.zsh
+++ node/init.zsh
@@ -1,8 +1,8 @@
 # disable browser launch after npm/yarn start
 export BROWSER=none
 
-alias vitest='npx vitest'
-alias sst='npx sst'
+alias vitest='pnpx vitest'
+alias sst='pnpx sst'
 
 export NVM_LAZY_LOAD=false
 zinit light lukechilds/zsh-nvm
