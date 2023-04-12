2089ff2bd98cb24d94c8125addd5fb35e5d3034b
Pieter Joost van de Sande <pj@born2code.net>
2023-04-12 22:53:57 +0200

    nvchad: switch to monekai theme

--- nvchad/custom/chadrc.lua
+++ nvchad/custom/chadrc.lua
@@ -1,8 +1,8 @@
 local M = {}
 
 M.ui = {
-	theme_toggle = { "tokyodark", "aye-light" },
-	theme = "tokyodark",
+	theme_toggle = { "monekai", "aye-light" },
+	theme = "monekai",
 	hl_override = {
 		Visual = { -- custom highlights are also allowed
 			bg = "#fffacd",
