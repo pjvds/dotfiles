c8ad185382ce12302acd26b56387f91fda053a27
Pieter Joost van de Sande <pj@born2code.net>
2023-08-02 12:40:29 +0200

    qmk: drop L_ADJUST layer to lower firmware file size

--- qmk/crkdb/born2code/keymap.c
+++ qmk/crkdb/born2code/keymap.c
@@ -53,8 +53,7 @@ enum layer_number {
   L_BYPASS,
   L_GAME  ,
   L_LOWER ,
-  L_RAISE ,
-  L_ADJUST,
+  L_RAISE
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -122,20 +121,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                           _______,   MO(L_ADJUST), _______,    _______, _______, KC_RALT
                                       //`--------------------------'  `--------------------------'
-  ),
-
-  [L_ADJUST] = LAYOUT_split_3x6_3(
-  //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-      QK_BOOT,TO(L_BASE),TO(L_BYPASS), TO(L_GAME), XXXXXXX, XXXXXXX,               XXXXXXX, XXXXXXX,  KC_INS, XXXXXXX, XXXXXXX, XXXXXXX,
-  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      RGB_TOG, RGB_HUI, RGB_SAI, RGB_VAI, XXXXXXX, XXXXXXX,                      XXXXXXX, CLEAR, XXXXXXX, KC_CAPS, XXXXXXX, XXXXXXX,
-  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      RGB_MOD, RGB_HUD, RGB_SAD, RGB_VAD, XXXXXXX, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-  //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                          _______, _______, _______,    _______, _______, KC_RALT
-                                      //`--------------------------'  `--------------------------'
   )
-
 };
 
 #ifdef OLED_DRIVER_ENABLE
