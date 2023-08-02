295962f345268ead087060f218aaef57aa0884d2
Pieter Joost van de Sande <pj@born2code.net>
2023-08-02 11:52:36 +0200

    qmk: remove IGNORE_MOD_TAP_INTERRUPT since it is the default behavior now

--- qmk/crkdb/born2code/config.h
+++ qmk/crkdb/born2code/config.h
@@ -32,9 +32,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define TAPPING_TERM 200
 
-// Prevent normal rollover on alphas from accidentally triggering mods.
-#define IGNORE_MOD_TAP_INTERRUPT
-
 // Enable rapid switch from tap to hold, disables double tap hold auto-repeat.
 #define TAPPING_FORCE_HOLD
 
