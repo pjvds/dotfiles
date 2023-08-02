09757f5e6a53bd52f3f2be81cee1902fe3b54bb2
Pieter Joost van de Sande <pj@born2code.net>
2023-08-02 15:57:54 +0200

    nvchad: custom backup location

--- nvchad/custom/init.lua
+++ nvchad/custom/init.lua
@@ -3,7 +3,15 @@ vim.g.mapleader = "'"
 -- don't use system clipboard
 vim.opt.clipboard = ""
 
+local opt = vim.opt
 local autocmd = vim.api.nvim_create_autocmd
+
+-- custom undo, swp and backup folder
+local prefix = vim.env.XDG_CONFIG_HOME or vim.fn.expand("~/.config/nvim")
+opt.undodir = { prefix .. "/.undo//"}
+opt.backup = true
+opt.backupdir = {prefix .. "/.backup//"}
+opt.swapfile = false
 
 -- Send SIGUSR1 signal to kitty after config file changed to reload the configuration
 autocmd("BufWritePost", {
