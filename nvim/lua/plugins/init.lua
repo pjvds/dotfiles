4de8934c1c28ed504fa2ac8fe4ce663f89c0d9f8
Pieter Joost van de Sande <pj@born2code.net>
2024-10-03 09:00:42 +0200

    nvim: add java support

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -8,6 +8,36 @@ return {
 		end,
 	},
 	{
+		"nvim-java/nvim-java",
+		lazy = false,
+		dependencies = {
+			"nvim-java/lua-async-await",
+			"nvim-java/nvim-java-core",
+			"nvim-java/nvim-java-test",
+			"nvim-java/nvim-java-dap",
+			"MunifTanjim/nui.nvim",
+			"neovim/nvim-lspconfig",
+			"mfussenegger/nvim-dap",
+			{
+				"williamboman/mason.nvim",
+				opts = {
+					registries = {
+						"github:nvim-java/mason-registry",
+						"github:mason-org/mason-registry",
+					},
+				},
+			},
+		},
+		config = function()
+			require("java").setup({})
+			require("lspconfig").jdtls.setup({
+				on_attach = require("nvchad.configs.lspconfig").on_attach,
+				capabilities = require("nvchad.configs.lspconfig").capabilities,
+				filetypes = { "java" },
+			})
+		end,
+	},
+	{
 		"stevearc/conform.nvim",
 		event = "BufWritePre", -- uncomment for format on save
 		config = function()
