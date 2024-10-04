8fbc38149c01005c301477dfc0963be5ecd152e3
Pieter Joost van de Sande <pj@born2code.net>
2024-10-04 23:59:47 +0200

    nvchad: enable java development integration

--- nvchad/custom/plugins.lua
+++ nvchad/custom/plugins.lua
@@ -9,6 +9,36 @@ local plugins = {
 		end,
 	},
 	{
+		"nvim-java/nvim-java",
+		lazy = false,
+		dependencies = {
+			"nvim-java/lua-async-await",
+			"nvim-java/nvim-java-core",
+			"nvim-java/nvim-java-test",
+			"nvim-java/nvim-java-dap",
+			"MunifTanjim/nui.nvim",
+			"neovim/nvim-lspconfig",
+			"mfussenegger/nvim-dap",
+			{
+				"williamboman/mason.nvim",
+				opts = {
+					registries = {
+						"github:nvim-java/mason-registry",
+						"github:mason-org/mason-registry",
+					},
+				},
+			},
+		},
+		config = function()
+			require("java").setup({})
+			require("lspconfig").jdtls.setup({
+				on_attach = require("nvchad.configs.lspconfig").on_attach,
+				capabilities = require("nvchad.configs.lspconfig").capabilities,
+				filetypes = { "java" },
+			})
+		end,
+	},
+	{
 		"RRethy/vim-illuminate",
 		event = "BufReadPost",
 		config = function()
