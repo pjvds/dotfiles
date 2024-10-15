e079d031a1ad915c3efe7b4ed77c2ff8793e4c29
Pieter Joost van de Sande <pj@born2code.net>
2024-10-15 11:38:58 +0200

    qmk: make born2code keymap compatible with latest qmk version

--- qmk/crkdb/born2code/keymap.c
+++ qmk/crkdb/born2code/keymap.c
@@ -138,11 +138,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 };
 
-#ifdef OLED_DRIVER_ENABLE
+#ifdef OLED_ENABLE
 oled_rotation_t oled_init_user(oled_rotation_t rotation) {
-  if (!is_master) {
-    return OLED_ROTATION_180;  // flips the display 180 degrees if offhand
-  }
+  //if (!is_keyboard_master) {
+  //  return OLED_ROTATION_180;  // flips the display 180 degrees if offhand
+  //}
   return rotation;
 }
 
@@ -150,7 +150,7 @@ void oled_render_layer_state(void) {
   uint8_t active_layer = get_highest_layer(layer_state);
 
   char buffer[12];
-  sprintf(buffer, "L:%s", active_layer);
+  sprintf(buffer, "L:%d", active_layer);
   oled_write_P(buffer, false);
 
   switch (active_layer) {
@@ -223,22 +223,14 @@ void render_bootmagic_status(bool status) {
     }
 }
 
-void oled_render_logo(void) {
-    static const char PROGMEM crkbd_logo[] = {
-        0x80, 0x81, 0x82, 0x83, 0x84, 0x85, 0x86, 0x87, 0x88, 0x89, 0x8a, 0x8b, 0x8c, 0x8d, 0x8e, 0x8f, 0x90, 0x91, 0x92, 0x93, 0x94,
-        0xa0, 0xa1, 0xa2, 0xa3, 0xa4, 0xa5, 0xa6, 0xa7, 0xa8, 0xa9, 0xaa, 0xab, 0xac, 0xad, 0xae, 0xaf, 0xb0, 0xb1, 0xb2, 0xb3, 0xb4,
-        0xc0, 0xc1, 0xc2, 0xc3, 0xc4, 0xc5, 0xc6, 0xc7, 0xc8, 0xc9, 0xca, 0xcb, 0xcc, 0xcd, 0xce, 0xcf, 0xd0, 0xd1, 0xd2, 0xd3, 0xd4,
-        0};
-    oled_write_P(crkbd_logo, false);
-}
-
-void oled_task_user(void) {
-    if (is_master) {
+bool oled_task_user(void) {
+    //if (is_keyboard_master) {
         oled_render_layer_state();
         oled_render_keylog();
-    } else {
-        oled_render_logo();
-    }
+    //} else {
+    //    oled_render_logo();
+    //}
+  return false;
 }
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
@@ -261,4 +253,4 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   }
   return true;
 }
-#endif // OLED_DRIVER_ENABLE
+#endif // OLED_ENABLE
