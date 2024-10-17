3a4e2014ced8a4fc19fa2f58f60b14e3d8720072
Pieter Joost van de Sande <pj@born2code.net>
2024-10-17 21:52:03 +0200

    nvim: fix incompatibility with latest base46 changes in nvchad

--- nvim/lua/chadrc.lua
+++ nvim/lua/chadrc.lua
@@ -1,11 +1,11 @@
--- This file  needs to have same structure as nvconfig.lua 
+-- This file  needs to have same structure as nvconfig.lua
 -- https://github.com/NvChad/NvChad/blob/v2.5/lua/nvconfig.lua
 
 ---@type ChadrcConfig
 local M = {}
 
-M.ui = {
-	theme = "onedark",
+M.base46 = {
+	theme = "palenight",
 
 	-- hl_override = {
 	-- 	Comment = { italic = true },
