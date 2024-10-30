e7ea25e4913b28f0de4b77a667f04f0b17f47e75
Pieter Joost van de Sande <pj@born2code.net>
2024-10-30 13:49:38 +0100

    base16: switch to palenight theme that is in line with nvim

--- base16/init.zsh
+++ base16/init.zsh
@@ -14,3 +14,5 @@ BASE16_SHELL="$HOME/.config/base16-shell/"
 [ -n "$PS1" ] && \
     [ -s "$BASE16_SHELL/profile_helper.sh" ] && \
         source "$BASE16_SHELL/profile_helper.sh"
+
+base16_material-palenight
