2114466d41396a87d1393f2b341bbe8913c3771f
Pieter Joost van de Sande <pj@born2code.net>
2024-11-04 11:21:59 +0000

    nvim: add leap.nvim for general purpose motions
    Thanks @alexandervantijffel for this tip!

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -11,6 +11,21 @@ return {
 			-- or run <leader>ch to see copilot mapping section
 		end,
 	},
+	{
+		"ggandor/leap.nvim",
+		keys = {
+			{ "s", mode = { "n", "x", "o" }, desc = "Leap forward to" },
+			{ "S", mode = { "n", "x", "o" }, desc = "Leap backward to" },
+			{ "gs", mode = { "n", "x", "o" }, desc = "Leap from windows" },
+		},
+		config = function(_, opts)
+			local leap = require("leap")
+			for k, v in pairs(opts) do
+				leap.opts[k] = v
+			end
+			leap.add_default_mappings(true)
+		end,
+	},
 	-- guess the indentation setting (tab vs spaces, indation level)
 	{
 		"nmac427/guess-indent.nvim",
