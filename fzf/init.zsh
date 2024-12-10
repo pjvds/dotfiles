0ae6e130de03a6c343a48e54b35c7f3c697ce6a0
Pieter Joost van de Sande <pj@born2code.net>
2024-12-10 11:31:58 +0100

    fzf: remove duplicate initialization

--- fzf/init.zsh
+++ fzf/init.zsh
@@ -2,16 +2,6 @@
 export FZF_DEFAULT_OPTS="--bind 'btab:toggle-up,tab:toggle-down' --cycle"
 export FZF_DEFAULT_COMMAND='ag --hidden --ignore .git -g ""'
 
-
-
-if [[ `uname` == "Darwin" ]]; then
-  source /opt/homebrew/opt/fzf/shell/key-bindings.zsh
-  source /opt/homebrew/opt/fzf/shell/completion.zsh
-else
-  source /usr/share/fzf/key-bindings.zsh
-  source /usr/share/fzf/completion.zsh
-fi
-
 zinit ice silent depth=1 wait"1"
 
 export FZF_UP_KEY='^K'
