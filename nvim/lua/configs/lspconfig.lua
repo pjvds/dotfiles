009faf960e5c9dd7129b7568e7fc19caff7a3fc1
Pieter Joost van de Sande <pj@born2code.net>
2025-07-08 09:56:36 +0200

    nvim: improve lsp config setup and add Vue support

--- nvim/lua/configs/lspconfig.lua
+++ nvim/lua/configs/lspconfig.lua
@@ -1,8 +1,6 @@
 -- load defaults i.e lua_lsp
 require("nvchad.configs.lspconfig").defaults()
 
-local lspconfig = require("lspconfig")
-
 -- EXAMPLE
 local servers = {
 	"html",
@@ -16,19 +14,14 @@ local servers = {
 	"angularls",
 	"yamlls",
 	"terraform-ls",
+	"vue_ls",
 }
 local nvlsp = require("nvchad.configs.lspconfig")
 
 -- lsps with default config
-for _, lsp in ipairs(servers) do
-	lspconfig[lsp].setup({
-		on_attach = nvlsp.on_attach,
-		on_init = nvlsp.on_init,
-		capabilities = nvlsp.capabilities,
-	})
-end
+vim.lsp.enable(servers)
 
-lspconfig.yamlls.setup({
+vim.lsp.config("yamlls", {
 	settings = {
 		yaml = {
 			schemas = {
@@ -37,6 +30,63 @@ lspconfig.yamlls.setup({
 		},
 	},
 })
+
+local vue_language_server_path = vim.fn.expand("$MASON/packages")
+	.. "/vue-language-server"
+	.. "/node_modules/@vue/language-server"
+
+local vue_plugin = {
+	name = "@vue/typescript-plugin",
+	location = vue_language_server_path,
+	languages = { "vue" },
+	configNamespace = "typescript",
+}
+local vtsls_config = {
+	settings = {
+		vtsls = {
+			tsserver = {
+				globalPlugins = {
+					vue_plugin,
+				},
+			},
+		},
+	},
+	filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" },
+}
+
+local vue_ls_config = {
+	on_init = function(client)
+		client.handlers["tsserver/request"] = function(_, result, context)
+			local clients = vim.lsp.get_clients({ bufnr = context.bufnr, name = "vtsls" })
+			if #clients == 0 then
+				vim.notify(
+					"Could not found `vtsls` lsp client, vue_lsp would not work without it.",
+					vim.log.levels.ERROR
+				)
+				return
+			end
+			local ts_client = clients[1]
+
+			local param = unpack(result)
+			local id, command, payload = unpack(param)
+			ts_client:exec_cmd({
+				title = "vue_request_forward", -- You can give title anything as it's used to represent a command in the UI, `:h Client:exec_cmd`
+				command = "typescript.tsserverRequest",
+				arguments = {
+					command,
+					payload,
+				},
+			}, { bufnr = context.bufnr }, function(_, r)
+				local response_data = { { id, r.body } }
+				---@diagnostic disable-next-line: param-type-mismatch
+				client:notify("tsserver/response", response_data)
+			end)
+		end
+	end,
+}
+
+vim.lsp.config("vtsls", vtsls_config)
+vim.lsp.config("vue_ls", vue_ls_config)
 
 -- configuring single server, example: typescript
 -- lspconfig.ts_ls.setup {
--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -2,6 +2,43 @@ return {
 	import = "nvchad.blink.lazyspec",
 	{ "vuciv/golf", event = "VeryLazy" },
 	{
+		"ramilito/winbar.nvim",
+		event = "VimEnter", -- Alternatively, BufReadPre if we don't care about the empty file when starting with 'nvim'
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		config = function()
+			require("winbar").setup({
+				-- your configuration comes here, for example:
+				icons = true,
+				diagnostics = true,
+				buf_modified = true,
+				buf_modified_symbol = "M",
+				-- or use an icon
+				-- buf_modified_symbol = "●"
+				dim_inactive = {
+					enabled = false,
+					highlight = "WinBarNC",
+					icons = true, -- whether to dim the icons
+					name = true, -- whether to dim the name
+				},
+			})
+		end,
+	},
+	{
+		"Bekaboo/dropbar.nvim",
+		event = "LspAttach",
+		-- optional, but required for fuzzy finder support
+		dependencies = {
+			"nvim-telescope/telescope-fzf-native.nvim",
+			build = "make",
+		},
+		config = function()
+			local dropbar_api = require("dropbar.api")
+			vim.keymap.set("n", "<Leader>;", dropbar_api.pick, { desc = "Pick symbols in winbar" })
+			vim.keymap.set("n", "[;", dropbar_api.goto_context_start, { desc = "Go to start of current context" })
+			vim.keymap.set("n", "];", dropbar_api.select_next_context, { desc = "Select next context" })
+		end,
+	},
+	{
 		"dmmulroy/ts-error-translator.nvim",
 		event = "LspAttach",
 		config = function()
