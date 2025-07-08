071c439f3c6a0a70e5d3dc7b02912d6ec9915c11
Pieter Joost van de Sande <pj@born2code.net>
2025-07-08 09:57:58 +0200

    fortify: add fortify as docker based tool

---
+++ fortify/init.zsh
@@ -0,0 +1,8 @@
+#local fortify_bin="/Users/pvandesande/Applications/Fortify/Fortify_Apps_and_Tools_24.4.0/bin"
+local fortify_bin="/Users/pvandesande/Applications/Fortify/Fortify_SCA_24.4.1/bin"
+if [ ! -d $fortify_bin ]; then
+  warn "Fortify not found at $fortify_bin"
+fi
+
+export PATH="$fortify_bin:$PATH"
+alias fcli='docker run --platform=linux/amd64 --rm -v "${HOME}:${HOME}" -v "${PWD}:${PWD}" -e "FCLI_USER_HOME=${HOME}" -e "FCLI_DEFAULT_SSC_URL=https://ssc.aat.deloitte.com/ssc" -w "${PWD}" -u $(id -u):$(id -g) fortifydocker/fcli'
