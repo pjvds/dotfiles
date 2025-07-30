4e1152f17da86335a53b1158d97091591b32a473
Pieter Joost van de Sande <pj@born2code.net>
2025-07-30 13:07:27 +0200

    android: add android configuration

---
+++ android/init.darwin.zsh
@@ -0,0 +1,7 @@
+if [ -d ~/Library/Android/sdk ]; then
+  export ANDROID_HOME=~/Library/Android/sdk
+  export PATH=$PATH:$ANDROID_HOME/emulator
+  export PATH=$PATH:$ANDROID_HOME/tools
+  export PATH=$PATH:$ANDROID_HOME/tools/bin
+  export PATH=$PATH:$ANDROID_HOME/platform-tools
+fi
