8bb0c88d041966547fd3ffd34d8f5aa4834e213c
Pieter Joost van de Sande <pj@born2code.net>
2025-09-03 09:52:21 +0200

    sketchybar: make workspace highlight more relyable

--- sketchybar/plugins/workspaces.sh
+++ sketchybar/plugins/workspaces.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
+
+# Get the list of active workspaces on the focused monitor
 ACTIVE_WORKSPACES=$(aerospace list-workspaces --monitor focused --empty no)
 
+# If this workspace is focused -> always show + highlight it
 if [ "$1" = "$AEROSPACE_FOCUSED_WORKSPACE" ]; then
-		sketchybar --set $NAME drawing=on
-    sketchybar --set $NAME background.drawing=on
+  sketchybar --set "$NAME" drawing=on
+  sketchybar --set "$NAME" background.drawing=on
+  exit 0
 fi
 
-if [ "$1" = "$AEROSPACE_PREV_WORKSPACE" ]; then
-    sketchybar --set $NAME background.drawing=off
-
-	if ! printf "%s\n" "${ACTIVE_WORKSPACES[@]}" | grep -Fxq -- "$1"; then
-		# If the previous workspace doesn't have any windows
-		sketchybar --set $NAME drawing=off
-	fi
+# If this workspace is not focused but still active -> show it (no background highlight)
+if printf "%s\n" "$ACTIVE_WORKSPACES" | grep -Fxq -- "$1"; then
+  sketchybar --set "$NAME" drawing=on
+  sketchybar --set "$NAME" background.drawing=off
+  exit 0
 fi
+
+# If this workspace has no windows and is not focused -> hide it completely
+sketchybar --set "$NAME" drawing=off
