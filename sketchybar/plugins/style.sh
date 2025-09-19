a29485c2283584f071ccbfa8a61cef58450a5f4c
Pieter Joost van de Sande <pj@born2code.net>
2025-09-19 17:18:19 +0200

    sketchybar: refactor styling code

--- sketchybar/plugins/style.sh
+++ sketchybar/plugins/style.sh
@@ -3,3 +3,9 @@
 
 export ICON_FONT="Hack Nerd Font:Bold:14.0"
 export LABEL_FONT="Hack Nerd Font:Bold:14.0"
+
+export WHITE_COLOR=0xffffffff
+
+export BAR_COLOR=0x40000000
+export ITEM_BG_COLOR=0x44ffffff
+export ACCENT_COLOR=0xff2cf9ed
