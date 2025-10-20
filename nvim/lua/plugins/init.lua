6b226fa671ad14e6c93332decc275a51179d3b05
Pieter Joost van de Sande <pj@born2code.net>
2025-10-20 12:18:00 +0200

    nvim: support opening single or multiple files

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -1,3 +1,35 @@
+local action_state = require("telescope.actions.state")
+local action_utils = require("telescope.actions.utils")
+local function single_or_multi_select(prompt_bufnr)
+	local current_picker = action_state.get_current_picker(prompt_bufnr)
+	local has_multi_selection = (next(current_picker:get_multi_selection()) ~= nil)
+
+	if has_multi_selection then
+		local results = {}
+		action_utils.map_selections(prompt_bufnr, function(selection)
+			table.insert(results, selection[1])
+		end)
+
+		-- load the selections into buffers list without switching to them
+		for _, filepath in ipairs(results) do
+			-- not the same as vim.fn.bufadd!
+			vim.cmd.badd({ args = { filepath } })
+		end
+
+		require("telescope.pickers").on_close_prompt(prompt_bufnr)
+
+		-- switch to newly loaded buffers if on an empty buffer
+		if vim.fn.bufname() == "" and not vim.bo.modified then
+			vim.cmd.bwipeout()
+			vim.cmd.buffer(results[1])
+		end
+		return
+	end
+
+	-- if does not have multi selection, open single file
+	require("telescope.actions").file_edit(prompt_bufnr)
+end
+
 return {
 	import = "nvchad.blink.lazyspec",
 	{ "vuciv/golf", event = "VeryLazy" },
@@ -179,13 +211,10 @@ return {
 				},
 			}
 
-			-- conf.defaults.mappings.i = {
-			--   ["<C-j>"] = require("telescope.actions").move_selection_next,
-			--   ["<Esc>"] = require("telescope.actions").close,
-			-- }
+			conf.defaults.mappings.i = {
+				["<cr>"] = single_or_multi_select,
+			}
 
-			-- or
-			-- table.insert(conf.defaults.mappings.i, your table)
 			return conf
 		end,
 	},
