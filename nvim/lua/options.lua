e8dfa281da125fd669ea4949fb7ce162b8a5b513
Pieter Joost van de Sande <pj@born2code.net>
2026-02-19 11:13:57 +0100

    nvim: always override selection color with pale soft yellow

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -5,3 +5,14 @@ local o = vim.o
 -- don't use the system clipboard
 -- ctrl+c will copy to the clipboard in visual mode
 o.clipboard = ""
+
+-- Set visual selection to custom yellow (RGB: 254, 248, 143)
+vim.api.nvim_set_hl(0, "Visual", { bg = "#FEF88F", fg = "#000000" })
+
+-- Ensure it persists after colorscheme loads
+vim.api.nvim_create_autocmd("ColorScheme", {
+	pattern = "*",
+	callback = function()
+		vim.api.nvim_set_hl(0, "Visual", { bg = "#FEF88F", fg = "#000000" })
+	end,
+})
