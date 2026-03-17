2806f49fbb5e38a17e350ce57829181b503c0d79
Pieter Joost van de Sande <pj@born2code.net>
2026-03-17 08:13:33 +0100

    nix: extract homebrew configuration to a dedicated module

--- darwin/default.nix
+++ darwin/default.nix
@@ -1,6 +1,7 @@
 { config, pkgs, lib, ... }: {
   imports = [
     ../modules/darwin/ui.nix
+    ../modules/darwin/homebrew.nix
   ];
 
   # Time Zone
@@ -65,69 +66,6 @@
     trackpad = {
       Clicking = true;  # Tap to click
     };
-  };
-
-  # Enable Homebrew management
-  homebrew = {
-    enable = true;
-    onActivation = {
-      autoUpdate = true;
-      cleanup = "zap"; # Removes unlisted formulas and casks
-      upgrade = true;
-    };
-
-    taps = [
-      "felixkratz/formulae"
-      "nikitabobko/tap"
-    ];
-
-    # GUI Applications (Casks)
-    casks = [
-      "alt-tab"
-      "android-studio"
-      "arc"
-      "aws-vault-binary"
-      "cursorcerer"
-      "cyberduck"
-      "deckset"
-      "discord"
-      "github"
-      "intellij-idea"
-      "kitty"
-      "lm-studio"
-      "maccy"
-      "microsoft-azure-storage-explorer"
-      "microsoft-edge"
-      "obsidian"
-      "philips-hue-sync"
-      "proton-mail"
-      "proton-pass"
-      "protonvpn"
-      "qmk-toolbox"
-      "raycast"
-      "rider"
-      "shortcat"
-      "shottr"
-      "sonos"
-      "visual-studio-code"
-      "zen"
-      "zwift"
-      
-      # Fonts
-      "font-fira-code-nerd-font"
-      "font-hack-nerd-font"
-      "font-inconsolata"
-      "font-iosevka-nerd-font"
-      "font-jetbrains-mono-nerd-font"
-      "font-monaspace"
-      "font-monaspice-nerd-font"
-      "font-sf-pro"
-      "font-sketchybar-app-font"
-    ];
-
-    brews = [
-      "switchaudio-osx"
-    ];
   };
 
   # Set system state version
---
+++ modules/darwin/homebrew.nix
@@ -0,0 +1,63 @@
+{ ... }: {
+  homebrew = {
+    enable = true;
+    onActivation = {
+      autoUpdate = true;
+      cleanup = "zap"; # Removes unlisted formulas and casks
+      upgrade = true;
+    };
+
+    taps = [
+      "felixkratz/formulae"
+      "nikitabobko/tap"
+    ];
+
+    # GUI Applications (Casks)
+    casks = [
+      "alt-tab"
+      "android-studio"
+      "arc"
+      "aws-vault-binary"
+      "cursorcerer"
+      "cyberduck"
+      "deckset"
+      "discord"
+      "github"
+      "intellij-idea"
+      "kitty"
+      "lm-studio"
+      "maccy"
+      "microsoft-azure-storage-explorer"
+      "microsoft-edge"
+      "obsidian"
+      "philips-hue-sync"
+      "proton-mail"
+      "proton-pass"
+      "protonvpn"
+      "qmk-toolbox"
+      "raycast"
+      "rider"
+      "shortcat"
+      "shottr"
+      "sonos"
+      "visual-studio-code"
+      "zen"
+      "zwift"
+      
+      # Fonts
+      "font-fira-code-nerd-font"
+      "font-hack-nerd-font"
+      "font-inconsolata"
+      "font-iosevka-nerd-font"
+      "font-jetbrains-mono-nerd-font"
+      "font-monaspace"
+      "font-monaspice-nerd-font"
+      "font-sf-pro"
+      "font-sketchybar-app-font"
+    ];
+
+    brews = [
+      "switchaudio-osx"
+    ];
+  };
+}
