09f788088016088f74332ff633362c42268c8176
Pieter Joost van de Sande <pj@born2code.net>
2026-03-17 12:26:06 +0100

    nix: add more keys to explicitly disable voice control

--- modules/darwin/dictation.nix
+++ modules/darwin/dictation.nix
@@ -2,12 +2,17 @@
   system.defaults.CustomUserPreferences = {
     "com.apple.assistant.support" = {
       "Dictation Enabled" = true;
+      "Voice Control Enabled" = false;
     };
     "com.apple.speech.recognition.AppleSpeechRecognition.prefs" = {
       DictationIMHasHandledEnablingDictation = true;
       DictationShortcuts = [ "Press Either Command Key Twice" ];
       DictationIMUseOnlyOfflineDictation = true;
       CACPersistentSleepState = false;
+      VoiceControlEnabled = false;
+    };
+    "com.apple.speech.voicecontrol.prefs" = {
+      Enabled = false;
     };
     "com.apple.symbolichotkeys" = {
       AppleSymbolicHotKeys = {
