87a7b9bd54b3efea6342d268a749a7d812968ec4
Pieter Joost van de Sande <pj@born2code.net>
2026-03-17 16:44:01 +0100

    zsh: move color aliases from nix to modular aliases.zsh

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,2 +1,9 @@
 alias qq='exit'
 alias :q='exit'
+
+# Use colors for ls and grep
+if [[ -z ${NO_COLOR} ]]; then
+  export CLICOLOR=1
+  export LSCOLORS="ExfxcxdxbxGxDxabagacad"
+  alias grep='grep --color=auto'
+fi
