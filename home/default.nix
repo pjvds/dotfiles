da4211cc683354df3d52b2eb2a030aafa57ab16a
Pieter Joost van de Sande <pj@born2code.net>
2026-03-17 13:51:47 +0100

    nix: add support for pjvds on Pieters-MacBook-Pro.local

--- home/default.nix
+++ home/default.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, ... }: {
+{ config, pkgs, lib, ... }: {
   imports = [
     ../modules/home/core.nix
     ../modules/home/languages.nix
@@ -15,8 +15,8 @@
 
   # Home Manager needs a bit of information about you and the paths it should manage
   home = {
-    username = "pvandesande";
-    homeDirectory = "/Users/pvandesande";
+    username = lib.mkDefault "pvandesande";
+    homeDirectory = lib.mkDefault "/Users/pvandesande";
     stateVersion = "24.11";
     
     # Packages to install
