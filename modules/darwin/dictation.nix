c9819d5fb4e7f28cb509533dcaf70fca1711ded6
Pieter Joost van de Sande <pj@born2code.net>
2026-03-17 10:19:29 +0100

    nix: extract dictation configuration into a dedicated darwin module

---
+++ modules/darwin/dictation.nix
@@ -0,0 +1,23 @@
+{ ... }: {
+  system.defaults.CustomUserPreferences = {
+    "com.apple.assistant.support" = {
+      "Dictation Enabled" = true;
+    };
+    "com.apple.speech.recognition.AppleSpeechRecognition.prefs" = {
+      DictationIMHasHandledEnablingDictation = true;
+      DictationShortcuts = [ "Press Command Key Twice" ];
+      DictationIMUseOnlyOfflineDictation = true;
+    };
+    "com.apple.symbolichotkeys" = {
+      AppleSymbolicHotKeys = {
+        "63" = {
+          enabled = true;
+          value = {
+            parameters = [ 65535 63 1048576 ];
+            type = "standard";
+          };
+        };
+      };
+    };
+  };
+}
