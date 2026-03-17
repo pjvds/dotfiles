cd6ced36290577d2000e6a975e5f8ada5c36102b
Pieter Joost van de Sande <pj@born2code.net>
2026-03-17 16:45:07 +0100

    zsh: move k8s autoloads to modular k8s/init.zsh

--- k8s/init.zsh
+++ k8s/init.zsh
@@ -1,14 +1,15 @@
 # k8s zim module - Kubernetes tools with lazy-loaded completions
 #
 # This module provides kubectl, minikube, k3d, and argo with lazy-loaded
-# shell completions. Completions are only sourced when you first use each tool,
-# significantly improving shell startup time.
+# completions and aliases.
 
 # Set up kubectl aliases
 alias k="kubectl --namespace dev-namespace"
 alias kd="kubectl --namespace dev-namespace"
 alias kt="kubectl --namespace test-namespace"
 
-# Note: The actual kubectl, minikube, k3d, argo, and kubeprintsec functions
+# Add functions to fpath and autoload them
+fpath=(${0:h}/functions $fpath)
+autoload -U argo k3d kubectl kubeprintsec minikube
 # are in the functions/ directory and will be autoloaded by zim when first used.
 # This provides lazy loading of expensive completion scripts.
