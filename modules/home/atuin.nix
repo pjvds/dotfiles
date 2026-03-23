1d9f9a8887ab0e1aa6dd8e4c4560bc31ad9a9a3e
Pieter Joost van de Sande <pj@born2code.net>
2026-03-23 08:11:52 +0100

    atuin: cleanup dangling symlink if it exists causing errors

--- modules/home/atuin.nix
+++ modules/home/atuin.nix
@@ -1,5 +1,14 @@
-{ pkgs, ... }: {
+{ pkgs, config, lib, ... }: {
   home.packages = [ pkgs.atuin ];
+
+  # If ~/.config/atuin is a symlink (e.g. leftover from the old mkOutOfStoreSymlink
+  # setup or a dangling GC'd store path), remove it before home-manager tries to
+  # create the directory. Without this, atuin fails with "File exists (os error 17)".
+  home.activation.cleanAtuinSymlink = lib.hm.dag.entryBefore [ "checkLinkTargets" ] ''
+    if [[ -L "${config.home.homeDirectory}/.config/atuin" ]]; then
+      rm "${config.home.homeDirectory}/.config/atuin"
+    fi
+  '';
 
   programs.atuin = {
     enable = true;
