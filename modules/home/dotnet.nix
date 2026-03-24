00a14669a39ddd4744c651b07fe82a8b3d403df8
Pieter Joost van de Sande <pj@born2code.net>
2026-03-24 10:41:24 +0100

    dotnet: switch to binary SDK and opt out of telemetry
    Use dotnetCorePackages.sdk_10_0-bin instead of dotnet-sdk_10 to avoid
    building from source, and set DOTNET_CLI_TELEMETRY_OPTOUT=1.

--- modules/home/dotnet.nix
+++ modules/home/dotnet.nix
@@ -1,6 +1,7 @@
-{ pkgs, ... }:
-{
-  home.packages = with pkgs; [
-    dotnet-sdk_10
-  ];
+{ pkgs, ... }: {
+  home.packages = [ pkgs.dotnetCorePackages.sdk_10_0-bin ];
+
+  home.sessionVariables = {
+    DOTNET_CLI_TELEMETRY_OPTOUT = "1";
+  };
 }
