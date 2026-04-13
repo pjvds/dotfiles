81be15854485dba6bbd8501d81efa4a4c1e1e31a
Pieter Joost van de Sande <pj@born2code.net>
2026-04-13 09:24:27 +0200

    nvim: update packages

--- modules/home/editor/config/nvim/lua/plugins/ui.lua
+++ modules/home/editor/config/nvim/lua/plugins/ui.lua
@@ -44,4 +44,14 @@ return {
 		dependencies = { "nvim-tree/nvim-web-devicons" },
 		opts = {},
 	},
+	{
+		"LucasGualtieri/visual-highlight.nvim",
+		config = function()
+			require("visual-highlight").setup({
+				-- Custom configuration (optional)
+				highlight_color = "#3a3a3a", -- Default highlight color
+			})
+		end,
+	},
 }
+
