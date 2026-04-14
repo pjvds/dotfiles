9e5e526bca96b66202d813e5b1507d2917c44288
Pieter Joost van de Sande <pj@born2code.net>
2026-04-14 11:45:23 +0200

    node: add fnv auto-switch support

--- modules/programs/node/default.nix
+++ modules/programs/node/default.nix
@@ -7,14 +7,21 @@ let cfg = config.my.node; in
   config = mkIf cfg.enable {
     home.packages = with pkgs; [ fnm ];
 
-    programs.fnm.enable = true;
-    programs.fnm.cacheEnvVars = true;
+    home.file.".npmrc" = {
+      source = config.lib.file.mkOutOfStoreSymlink
+        "${config.home.homeDirectory}/dotfiles/modules/programs/node/config/.npmrc";
+      force = true;
+    };
 
-    home.file.".npmrc".source = config.lib.file.mkOutOfStoreSymlink
-      "${config.home.homeDirectory}/dotfiles/modules/programs/node/config/.npmrc";
+    programs.zsh = {
+      shellAliases = {
+        sst = "bunx sst";
+      };
 
-    programs.zsh.shellAliases = {
-      sst = "bunx sst";
+      initContent = lib.mkOrder 105 ''
+        # fnm (Fast Node Manager) — auto-switch Node versions
+        eval "$(fnm env --use-on-cd)"
+      '';
     };
   };
 }
