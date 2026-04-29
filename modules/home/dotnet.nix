0998e34a6623304b1a6c75ef7bdcc1a72e53899f
Pieter Joost van de Sande <pj@born2code.net>
2026-04-29 10:02:29 +0200

    dotnet: fix dotnet env vars not pointing to share subfolder

--- modules/home/dotnet.nix
+++ modules/home/dotnet.nix
@@ -2,26 +2,25 @@
 let
   cfg = config.my.dotnet;
   dotnet = pkgs.dotnetCorePackages;
+  dotnetPkg = dotnet.combinePackages [
+    dotnet.sdk_9_0-bin
+    dotnet.sdk_10_0-bin
+  ];
 in
 {
   options.my.dotnet.enable = lib.mkEnableOption ".NET SDK";
 
   config = lib.mkIf cfg.enable {
-    home.packages = [
-      (dotnet.combinePackages [
-        dotnet.sdk_9_0-bin
-        dotnet.sdk_10_0-bin
-      ])
-    ];
+    home.packages = [ dotnetPkg ];
 
     home.sessionVariables = {
       DOTNET_CLI_TELEMETRY_OPTOUT = "1";
     };
 
     programs.zsh.initContent = ''
-      export DOTNET_ROOT="$HOME/.dotnet"
-      export DOTNET_TOOLS="$DOTNET_ROOT/tools"
-      export PATH="$PATH:$DOTNET_ROOT:$DOTNET_TOOLS"
+      export DOTNET_ROOT="${dotnetPkg}/share/dotnet"
+      export DOTNET_TOOLS="$HOME/.dotnet/tools"
+      export PATH="$PATH:$DOTNET_TOOLS"
     '';
   };
 }
