3b4b43a9badfd2dacc2d03ccdccc6520a6546509
Pieter Joost van de Sande <pj@born2code.net>
2026-04-30 11:03:47 +0200

    theme: add sketchybar and jankyborders theming

--- modules/home/theme/themes/sketchybar/light.sh
+++ modules/home/theme/themes/sketchybar/light.sh
@@ -1,8 +1,8 @@
 export ICON_FONT="Hack Nerd Font:Bold:14.0"
 export LABEL_FONT="Hack Nerd Font:Bold:14.0"
 
-export WHITE_COLOR=0xff403f53
+export WHITE_COLOR=0xff1a1a1a
 
-export BAR_COLOR=0x40f0f0f0
-export ITEM_BG_COLOR=0x44000000
-export ACCENT_COLOR=0xff288ed7
+export BAR_COLOR=0xe5f5f5f5
+export ITEM_BG_COLOR=0x22000000
+export ACCENT_COLOR=0xff0066cc
