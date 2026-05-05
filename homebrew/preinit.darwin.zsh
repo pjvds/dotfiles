8cbbc57be175028ceff4c7a7308993b862984acb
Pieter Joost van de Sande <pj@born2code.net>
2026-05-05 09:21:08 +0200

    homebrew: drop root module

--- homebrew/preinit.darwin.zsh
+++
@@ -1,8 +0,0 @@
-if [[ ! -f /opt/homebrew/bin/brew ]]; then
-	error "Homebrew not found"
-  return 0
-fi
-
-# install casks in user application directory
-export HOMEBREW_CASK_OPTS="--appdir=~/Applications"
-eval "$(/opt/homebrew/bin/brew shellenv)"
