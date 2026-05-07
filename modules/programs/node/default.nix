32af638297f1bccdf37f8e0d8da638249a1d4556
Pieter Joost van de Sande <pj@born2code.net>
2026-05-07 12:15:12 +0200

    node: pin default Node.js to v24 via fnm activation
    Copilot language server requires node:sqlite which is only available
    by default in Node.js v23.4+. Pin to v24 LTS to ensure compatibility.

--- modules/programs/node/default.nix
+++ modules/programs/node/default.nix
@@ -7,6 +7,13 @@ let cfg = config.my.node; in
   config = mkIf cfg.enable {
     home.packages = with pkgs; [ fnm ];
 
+    home.activation.installNode = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+      if ! ${pkgs.fnm}/bin/fnm list 2>/dev/null | grep -q "v24"; then
+        $DRY_RUN_CMD ${pkgs.fnm}/bin/fnm install 24
+      fi
+      $DRY_RUN_CMD ${pkgs.fnm}/bin/fnm default 24
+    '';
+
     home.file.".npmrc" = {
       source = config.lib.file.mkOutOfStoreSymlink
         "${config.home.homeDirectory}/dotfiles/modules/programs/node/config/.npmrc";
