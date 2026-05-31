69e833af43d6d653f2f40c617c105c16b2b96319
Pieter Joost van de Sande <pj@born2code.net>
2026-05-31 11:45:37 +0200

    python: drop micromamba

--- modules/home/python.nix
+++ modules/home/python.nix
@@ -1,7 +1,7 @@
 { pkgs, config, lib, ... }:
 let cfg = config.my.python; in
 {
-  options.my.python.enable = lib.mkEnableOption "Python, pyenv, and micromamba";
+  options.my.python.enable = lib.mkEnableOption "Python and pyenv";
 
   config = lib.mkIf cfg.enable {
     programs.pyenv.enable = true;
@@ -10,15 +10,8 @@ let cfg = config.my.python; in
       python3
       (pipx.overrideAttrs (_: { doInstallCheck = false; }))
       pipenv
-      micromamba
     ];
 
-    # Session variables for Micromamba
-    home.sessionVariables = {
-      MAMBA_NO_LOW_SPEED_LIMIT = "1";
-    };
-
-    # Declarative Micromamba initialization
     programs.zsh = {
       shellAliases = {
         pip = "pip3";
@@ -29,15 +22,6 @@ let cfg = config.my.python; in
 
         # Pipx
         export PATH="$PATH:$HOME/.local/bin"
-
-        # Enable Micromamba shell hook without modifying PROMPT
-        # Powerlevel10k has built-in support for the anaconda segment which handles mamba
-        if command -v micromamba &>/dev/null; then
-          export MAMBA_ROOT_PREFIX="$HOME/.micromamba"
-          # Suppress PROMPT modification from mamba; let p10k handle it
-          export MAMBA_NO_PROMPT=1
-          eval "$(micromamba shell hook --shell zsh)"
-        fi
       '';
     };
   };
